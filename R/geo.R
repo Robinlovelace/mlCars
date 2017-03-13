ab354fc60aa6de7df1550644d1a1a8cf1503e3c8
Robin Lovelace <rob00x@gmail.com>
2017-03-13 17:24:32 +0000

    Add parks initial code

--- R/geo.R
+++ R/geo.R
@@ -20,23 +20,28 @@ wyflows = wyflows[wyflows$distance > 0,]
 library(osmdata)
 q_motorways = opq(bbox = bbox(wyflows)) %>%
   add_feature(key = "highway", value = "motorway")
+
 motorways = osmdata_sp(q_motorways)
 (m1 = m + qtm(motorways$osm_points))
 sel_mways = seq(from = 1, to = nrow(motorways$osm_poi), by = 100)
 m + qtm(motorways$osm_points[sel_mways,])
-l_points = stplanr::line2points(wyflows) # takes a while to run
+# l_points = stplanr::line2points(wyflows) # takes a while to run
 
-
-spDistsN1(wyflows, motorways$osm_points[sel_mways,])
+# spDistsN1(wyflows, motorways$osm_points[sel_mways,])
 wyflow_proj = spTransform(wyflows, CRS("+init=epsg:27700"))
 mpoints_proj = spTransform(motorways$osm_points[sel_mways,], CRS("+init=epsg:27700"))
 
-dm = rgeos::gDistance(wyflow_proj, mpoints_proj, byid = T, )
+dm = rgeos::gDistance(wyflow_proj, mpoints_proj, byid = T)
 dm = unlist(dm)
 dmin = apply(dm, 2, min)
 
-
 # parks
+q_parks_l = opq(bbox = bbox(wyflows)) %>%
+  add_feature(key = "leisure", value = "park")
+parks = osmdata_sp(q_parks_l)
+plot(parks$osm_polygons)
+qtm(parks$osm_points)
+
 # bus stops
 
 
